2cc2257fca01bff34b7f2ec35b87b1ce55ef3cbd
João Paulo Rocon <104106279+JoaoPauloRocon@users.noreply.github.com>
2025-04-10 08:54:39 -0300

    TR-003 Incremento
    Sequelize, sequelize-cli e mysql2 instalados. Criada a conexão em application/config/database.js, .sequelizerc para estruturar caminhos conforme arquitetura. Estrutura pronta para migrations e seeders.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // server.js
 const app = require('./app');
 const dotenv = require('dotenv');
+const sequelize = require('./application/config/database');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+
+
+sequelize.authenticate()
+  .then(() => console.log('✅ Conectado ao MySQL com Sequelize!'))
+  .catch(err => console.error('❌ Erro ao conectar:', err));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
