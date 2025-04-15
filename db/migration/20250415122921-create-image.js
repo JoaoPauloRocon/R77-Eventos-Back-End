18b857de45145c48194f8afca1127669c4b9859d
João Paulo Rocon <104106279+JoaoPauloRocon@users.noreply.github.com>
2025-04-15 13:25:37 -0300

    TR-014 Incremento
    - Criada a entidade Image com os campos: id, filename, url, event_id, uploadedAt.
    - Relacionamento com a entidade Event.
    - Preparação para upload de imagens.

---
+++ db/migration/20250415122921-create-image.js
@@ -0,0 +1,41 @@
+// src/core/db/migrations/XXXXXX-create-image.js
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('images', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      event_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'events',
+          key: 'id',
+        },
+        onDelete: 'CASCADE', // Ao deletar um evento, as imagens associadas serão deletadas
+      },
+      uploadedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('images');
+  },
+};
